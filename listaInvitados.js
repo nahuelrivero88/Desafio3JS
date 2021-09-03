3f60ff5badfa6526bbb2ebbe20671e2db0f17316
Nahuel Rivero <nahuelriveroch@gmail.com>
2021-09-03 00:48:41 -0300

    Entrega desafio

---
+++ listaInvitados.js
@@ -0,0 +1,19 @@
+// Lista de inivitados
+
+for (let i = 1; i <= 10; i++) {
+   let ingresarInvitado = prompt("Ingresar nombre del invitado:");
+   alert("N° de invitado: "+ i + "\nNombre: "+ ingresarInvitado);
+
+   if (i == 10){
+      alert("La lista de invitados esta completa");
+   }
+   else if (ingresarInvitado == "") {
+      alert("No ingreso el nombre correctamente, debe ingresarlo otra vez");
+      ingresarInvitado = prompt("Ingresar nombre del invitado:");
+      alert("N° de invitado: "+ i + "\nNombre: "+ ingresarInvitado);
+   }
+   else if (ingresarInvitado == "Nicolas" || ingresarInvitado == "Lucia") {
+      alert ("No se pudo agregar a esta persona, se encuentra en la lista negra.");
+      continue;
+   }
+}
